f1a0efb7d253ed5c3abdee9fc22f96c3586ab392
Demo User <debian@beaglebone.localdomain>
2017-10-31 17:30:14 +0000

    round check

--- apitest.py
+++ apitest.py
@@ -1,4 +1,4 @@
-import httplib, urllib, base64, json
+import httplib, urllib, base64, json, time
 
 ###############################################
 #### Update or verify the following values. ###
@@ -16,84 +16,93 @@ subscription_key = '97ce17233dbb4b9ab1189e344cf63dd4'
 # NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
 # a free trial subscription key, you should not need to change this region.
 uri_base = 'westcentralus.api.cognitive.microsoft.com'
+for i in range (1,5): 
+    print("wsb"+ str(i))
 
-# Request headers.
-headers_octet = {
-    'Content-Type': 'application/octet-stream',
-    'Ocp-Apim-Subscription-Key': subscription_key,
-}
-
-headers_json = {
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': subscription_key,
-}
-
-# Request parameters.
-params = urllib.urlencode({
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': '',
-})
-
-# The URL of a JPEG image to analyze.
-# body1 = "{'url':'http://hairstyles.thehairstyler.com/hairstyle_views/front_view_images/4603/original/Zach-Quinto.jpg'}"
-# body2 = "{'url':'https://d24v5oonnj2ncn.cloudfront.net/wp-content/uploads/2015/06/11095458/Eli-Roth.jpg'}"
-
-body1 = ""
-filename = '/home/debian/ECE497_Final/wcx.jpg'
-f = open(filename, "rb")
-body1 = f.read()
-f.close()
-
-body2 = ""
-filename = '/home/debian/ECE497_Final/wcx1.jpg'
-f = open(filename, "rb")
-body2 = f.read()
-f.close()
-
-try:
-    # Execute the REST API call and get the response.
-    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/face/v1.0/detect?%s" % params, body1, headers_octet)
-    response1 = conn.getresponse()
-    data1 = response1.read()
-
-    # 'data' contains the JSON data. The following formats the JSON data for display.
-    parsed1 = json.loads(data1)
-    print ("Response:")
-    # print (json.dumps(parsed, sort_keys=True, indent=2))
-    print(parsed1[0]['faceId'])
-    id1 = parsed1[0]['faceId']
-    # conn.close()
+    # Request headers.
+    headers_octet = {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
     
-    # conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/face/v1.0/detect?%s" % params, body2, headers_octet)
-    response2 = conn.getresponse()
-    data2 = response2.read()
-
-    # 'data' contains the JSON data. The following formats the JSON data for display.
-    parsed2 = json.loads(data2)
-    print ("Response:")
-    # print (json.dumps(parsed, sort_keys=True, indent=2))
-    print(parsed2[0]['faceId'])
-    id2 = parsed2[0]['faceId']
+    headers_json = {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
     
-    params3 = urllib.urlencode({
-})
-    body = "{'faceId1':'"+id1+"','faceId2':'"+id2+"'}"
-    print(body)
+    # Request parameters.
+    params = urllib.urlencode({
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': '',
+    })
     
-    conn.request("POST", "/face/v1.0/verify?%s" %params3, body, headers_json)
-    response = conn.getresponse()
-    data = response.read()
-
-    # 'data' contains the JSON data. The following formats the JSON data for display.
-    parsed = json.loads(data)
-    print ("Response:")
-    print (json.dumps(parsed, sort_keys=True, indent=2))
+    # The URL of a JPEG image to analyze.
+    # body1 = "{'url':'http://hairstyles.thehairstyler.com/hairstyle_views/front_view_images/4603/original/Zach-Quinto.jpg'}"
+    # body2 = "{'url':'https://d24v5oonnj2ncn.cloudfront.net/wp-content/uploads/2015/06/11095458/Eli-Roth.jpg'}"
     
-    conn.close()
+    body1 = ""
+    filename = '/home/debian/final/ECE497_Final/'+str(i)+'.jpg'
+    f = open(filename, "rb")
+    body1 = f.read()
+    f.close()
+    
+    body2 = ""
+    filename = '/home/debian/final/ECE497_Final/wcx1.jpg'
+    f = open(filename, "rb")
+    body2 = f.read()
+    f.close()
+    
+    try:
+        # Execute the REST API call and get the response.
+        conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+        conn.request("POST", "/face/v1.0/detect?%s" % params, body1, headers_octet)
+        response1 = conn.getresponse()
+        data1 = response1.read()
+    
+        # 'data' contains the JSON data. The following formats the JSON data for display.
+        parsed1 = json.loads(data1)
+        print ("Response:")
+        # print (json.dumps(parsed, sort_keys=True, indent=2))
+        print(parsed1[0]['faceId'])
+        id1 = parsed1[0]['faceId']
+        # conn.close()
+        
+        # conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+        conn.request("POST", "/face/v1.0/detect?%s" % params, body2, headers_octet)
+        response2 = conn.getresponse()
+        data2 = response2.read()
+    
+        # 'data' contains the JSON data. The following formats the JSON data for display.
+        parsed2 = json.loads(data2)
+        print ("Response:")
+        # print (json.dumps(parsed, sort_keys=True, indent=2))
+        print(parsed2[0]['faceId'])
+        id2 = parsed2[0]['faceId']
+        
+        params3 = urllib.urlencode({
+        })
+        body = "{'faceId1':'"+id1+"','faceId2':'"+id2+"'}"
+        print(body)
+        
+        conn.request("POST", "/face/v1.0/verify?%s" %params3, body, headers_json)
+        response = conn.getresponse()
+        data = response.read()
+    
+        # 'data' contains the JSON data. The following formats the JSON data for display.
+        parsed = json.loads(data)
+        print ("Response:")
+        print (json.dumps(parsed, sort_keys=True, indent=2))
+        print(parsed[1]['isIdentical'])
+        # if(parsed[1]['isIdentical'] == 'true'):
+        #     print("WELCOME")
+        #     break
+        # else:
+        conn.close()
+    
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
+    time.sleep(2)
+    
+# conn.close()
