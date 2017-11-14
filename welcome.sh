2608828d1b7ce89ad7bcb93d4abffdab4618a6c6
Demo User <debian@beaglebone.localdomain>
2017-11-14 14:40:11 +0000

    Improve instruction

--- welcome.sh
+++ welcome.sh
@@ -9,7 +9,7 @@ sudo ./on.sh
 # From: http://www.imagemagick.org/Usage/text/
 convert -background Khaki -font Times-Roman -pointsize 20 \
       -size $SIZE -gravity center\
-      label:'Please look directly into the camera\nPress GREEN button to add a new face\nPress RED button to verify identity' \
+      label:'Please look directly into the camera\n\nPress GREEN button and wait the red\nLED to flash once to add a new face\n\nPress RED button to verify identity' \
       $TMP_FILE
 
 sudo fbi -noverbose -T 1 $TMP_FILE
